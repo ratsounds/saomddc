06d77069622c2734d41be14c91d00f36eb41a4cf
ratsounds <ratsounds@gmail.com>
2018-07-17 21:54:18 +0900

    update for new units

--- js/ui.js
+++ js/ui.js
@@ -418,8 +418,8 @@ function calcRanking() {
             ranking.push(dcv);
             dcv.combo_speed_rate = (1 - dcv.sv.c.combo_speed * Math.floor(boss.combo / 10));
             dcv.duration = dcv.sv.c.s3_duration * dcv.combo_speed_rate;
-            dcv.cduration = dcv.sv.c.s3_c_duration ? dcv.sv.c.s3_c_duration : Infinity;
-            dcv.c2duration = dcv.sv.c.s3_c_duration ? dcv.sv.c.s3_c_duration : dcv.sv.c.s3_duration * dcv.combo_speed_rate;
+            dcv.cduration = dcv.sv.c.s3_c_duration ? dcv.sv.c.s3_c_duration * dcv.combo_speed_rate: Infinity;
+            dcv.c2duration = dcv.duration + dcv.sv.c.s3_c_duration ? dcv.cduration : dcv.duration;
             dcv.duration_50 = dcv.sv.c.s3_duration * (1 - dcv.sv.c.combo_speed * Math.floor(50 / 10));
             dcv.dps = Math.floor(dcv.damage / dcv.duration);
             dcv.dpm = Math.floor(getDPM(dcv));
@@ -428,7 +428,7 @@ function calcRanking() {
             dcv.duration = Math.floor(dcv.duration * 100) / 100;
             dcv.duration_50 = Math.floor(dcv.duration_50 * 100) / 100;
             dcv.cduration = Math.floor(dcv.cduration * 100) / 100;
-            dcv.c2duration = Math.floor((dcv.c2duration + dcv.duration) * 100) / 100;
+            dcv.c2duration = Math.floor(dcv.c2duration * 100) / 100;
             dcv.gap = Math.floor((dcv.duration - dcv.cduration) * 100) / 100;
             dcv.capacity = Math.floor(dcv.damage * dcv.sv.mp / dcv.sv.cost);
             dcv.damage = Math.floor(dcv.damage);
