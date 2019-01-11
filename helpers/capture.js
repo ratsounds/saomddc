e7f49bfb2bcdc3d0ad097846827fec6b69077046
ratsounds <ratsounds@gmail.com>
2019-01-11 21:36:05 +0900

    helper script for multi column screenshot

---
+++ helpers/capture.js
@@ -0,0 +1,65 @@
+/**
+ * this script require puppeteer and jimp
+ * this script require puppeteer and jimp.
+ */
+
+const puppeteer = require('puppeteer');
+const Jimp = require("jimp");
+
+/**
+ * get a full size screenshot from given url as multi column image.
+ * @param {string} url - target url
+ * @param {string} file - file path for output image
+ * @param {number} max_height - max height of screenshot
+ * @param {number} overrap - overrap pixel size for each column
+ * @return {Promise} this function returns promise with Jimp image object of destination
+ */
+async function capture(url, file, src_width, max_height, overrap) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setViewport({ width: src_width, height: src_width });
+  await page.goto(url, { waitUntil: 'networkidle2' });
+  await page.screenshot({ path: file, fullPage: true });
+  await browser.close();
+  await convertIntoMultiColumn(file, file, max_height, overrap);
+}
+/**
+ * convert vertically long image into multi column image.
+ * @param {string} src_path - file path for source image
+ * @param {string} dst_path - file path for destination image
+ * @param {number} max_height - height of destination image
+ * @param {number} overrap - overrap pixel size for each column
+ * @return {Promise} this function returns promise with Jimp image object of destination
+ */
+function convertIntoMultiColumn(src_path, dst_path, max_height, overrap) {
+  return new Promise((resolve, reject) => {
+    Jimp.read(src_path, (err, src) => {
+      if (err) { reject(err) }
+      let w = 0;
+      let h = max_height;
+      let cy = 0;
+      let lines = [];
+      while (cy < src.bitmap.height) {
+        lines.push({
+          dx: w,
+          sy: cy
+        })
+        w += src.bitmap.width;
+        cy += max_height - overrap;
+      }
+      new Jimp(w, h, (err, dst) => {
+        if (err) { reject(err) }
+        for (let i = 0; i < lines.length; i++) {
+          const line = lines[i];
+          dst.blit(src, line.dx, 0, 0, line.sy, src.bitmap.width, max_height);
+        }
+        dst.write(dst_path, (err) => {
+          if (err) { reject(err) }
+          resolve(dst);
+        })
+      });
+    });
+  });
+}
+
+module.exports = capture;
---
+++ helpers/capture_test.js
@@ -0,0 +1,16 @@
+const capture = require('./capture.js');
+/**
+ * get a full size screenshot from given url as multi column image.
+ * @param {string} url - target url
+ * @param {string} file - file path for output image
+ * @param {number} max_height - max height of screenshot
+ * @param {number} overrap - overrap pixel size for each column
+ * @return {Promise} this function returns promise with Jimp image object of destination
+ */
+capture(
+  'https://api-defrag.wrightflyer.net/webview/announcement-detail?id=490703&phone_type=2',
+  'info.png',
+  1024,
+  4096,
+  32
+);
