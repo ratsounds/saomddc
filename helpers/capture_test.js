6f6259ba101580a0e714d56f76fe3b7e84dbd3d1
ratsounds <ratsounds@gmail.com>
2019-01-11 21:41:08 +0900

    helper script for multi column screenshot

--- helpers/capture_test.js
+++ helpers/capture_test.js
@@ -3,6 +3,7 @@ const capture = require('./capture.js');
  * get a full size screenshot from given url as multi column image.
  * @param {string} url - target url
  * @param {string} file - file path for output image
+ * @param {number} src_width - width for source screenshot
  * @param {number} max_height - max height of screenshot
  * @param {number} overrap - overrap pixel size for each column
  */
