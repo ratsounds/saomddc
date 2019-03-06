2093f0db0e3fb8b72fd575146f346031f991703c
ratsounds <ratsounds@gmail.com>
2019-03-06 21:18:33 +0900

    equipment csv files are added

--- data/list.js
+++ data/list.js
@@ -1,13 +1,37 @@
 /* 
-  Helper Nodejs Script to make unit name csv file from data.json
+  Helper Nodejs Script to make unit and equipment name csv file from data.json
 */
 
 const fs = require('fs');
 const data = require('./data.json');
-const csv = [['name','name_en','group','group_en'].join(',')];
-for(let id in data.base) {
+
+function writeCSV(filename, src, props) {
+  const out = [props.join(',')];
+  for (let id in src) {
+    const item = src[id];
+    const line = [];
+    for (let p = 0; p < props.length; p++) {
+      line.push(item[props[p]]);
+    }
+    out.push('"' + line.join('","') + '"');
+  }
+  fs.writeFileSync(filename, out.join('\n'), 'utf8');
+}
+
+// unit list
+const csv = [
+  ['name', 'name_en', 'group', 'group_en'].join(',')
+];
+for (let id in data.base) {
   const unit = data.base[id];
   const group = data.group[unit.group];
-  csv.push('"'+[unit.name, unit.name_en, group.long, group.long_en].join('","')+'"');
+  csv.push('"' + [unit.name, unit.name_en, group.long, group.long_en].join('","') + '"');
 }
-fs.writeFileSync('list.csv',csv.join('\n'),'utf8');
+fs.writeFileSync('unit+group.csv', csv.join('\n'), 'utf8');
+
+// weapon list
+writeCSV('weapon.csv', data.weapon, ['name', 'name_en']);
+// armor list 
+writeCSV('armor.csv', data.armor, ['name', 'name_en']);
+// accesorry list 
+writeCSV('accesorry.csv', data.accesorry, ['name', 'name_en']);
